46954b5a4ac93eb5e168fe39b056224d90b14f6c
thaianhtran <thaianhtran2311@gmail.com>
2022-06-15 16:42:50 +0700

    ádsa

---
+++ app/Models/chitietyeucau.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class chitietyeucau extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id_yc',
+        'id_nguoithuchien',
+        'id_chucvu',
+    ];
+
+    public function ct_nth(){
+        return $this->hasOne(nguoithuchien::class, 'id', 'id_nguoithuchien');
+    }
+
+}
